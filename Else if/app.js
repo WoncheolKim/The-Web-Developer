214318e30175588a83f405fe899e648c1352ca14
Woncheol Kim <icijiy@gmail.com>
2023-01-20 22:25:44 -0800

    Practice with Else-if 02

--- Else if/app.js
+++ Else if/app.js
@@ -1,9 +1,24 @@
-const dayOfWeek = 'Friday';
+// const dayOfWeek = 'Friday';
 
-if (dayOfWeek === 'Monday') {
-  console.log("I hate Monday!");
-} else if (dayOfWeek === 'Saturday') {
-  console.log("I love Saturday");
-} else if (dayOfWeek === "Friday") {
-  console.log("Friday are decent, especially after work!");
+// if (dayOfWeek === 'Monday') {
+//   console.log("I hate Monday!");
+// } else if (dayOfWeek === 'Saturday') {
+//   console.log("I love Saturday");
+// } else if (dayOfWeek === "Friday") {
+//   console.log("Friday are decent, especially after work!")
+// }
+
+// 0-5 Free
+// 5-10 Child $10
+// 11-65 Adult $20
+// 65+ Senior $10
+
+const age = 8;
+
+if (age < 5) {
+  console.log("You are a baby. You get in for free!");
+} else if (age < 10) {
+  console.log("You are a child. You pay $10");
+} else if (age < 65) {
+  console.log("You are an adult. You pay $20");
 }
