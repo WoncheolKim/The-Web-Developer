77c4d966c1e30b941e3ce1b41a32ad2da9b0669c
Woncheol Kim <icijiy@gmail.com>
2023-01-20 22:30:40 -0800

    Practice with Else 01

--- Else if/app.js
+++ Else if/app.js
@@ -1,24 +1,30 @@
-// const dayOfWeek = 'Friday';
+const dayOfWeek = 'Tuesday';
 
-// if (dayOfWeek === 'Monday') {
-//   console.log("I hate Monday!");
-// } else if (dayOfWeek === 'Saturday') {
-//   console.log("I love Saturday");
-// } else if (dayOfWeek === "Friday") {
-//   console.log("Friday are decent, especially after work!")
-// }
+if (dayOfWeek === 'Monday') {
+  console.log("I hate Monday!");
+} else if (dayOfWeek === 'Saturday') {
+  console.log("I love Saturday");
+} else if (dayOfWeek === "Friday") {
+  console.log("Friday are decent, especially after work!");
+} else if (dayOfWeek === "Tuesday") {
+  console.log("MEH");
+} else if (dayOfWeek === "Wednesday") {
+  console.log("MEH");
+} else if (dayOfWeek === "Thursday") {
+  console.log("MEH");
+}
 
 // 0-5 Free
 // 5-10 Child $10
 // 11-65 Adult $20
 // 65+ Senior $10
 
-const age = 8;
+// const age = 8;
 
-if (age < 5) {
-  console.log("You are a baby. You get in for free!");
-} else if (age < 10) {
-  console.log("You are a child. You pay $10");
-} else if (age < 65) {
-  console.log("You are an adult. You pay $20");
-}
+// if (age < 5) {
+//   console.log("You are a baby. You get in for free!");
+// } else if (age < 10) {
+//   console.log("You are a child. You pay $10");
+// } else if (age < 65) {
+//   console.log("You are an adult. You pay $20");
+// }
