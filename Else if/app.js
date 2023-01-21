2de7d7058b41c0fcd5ff53bb06ce6a0a3d5414bd
Woncheol Kim <icijiy@gmail.com>
2023-01-20 22:17:25 -0800

    Practice with if 02

--- Else if/app.js
+++ Else if/app.js
@@ -1,6 +1,4 @@
-console.log("Before Conditional");
-let num = 10;
-if (num === 10) {
-  console.log("Math still works!");
+let random = Math.random();
+if (random > 0.5) {
+  console.log("Your number is greater than 0.5");
 }
-console.log("After Conditional");
