a335af508be79e42cf18abfaf7190fc5e03a8d75
Woncheol Kim <icijiy@gmail.com>
2023-01-26 17:58:49 -0800

    Practice with if condition 02

--- Else if/app.js
+++ Else if/app.js
@@ -35,15 +35,21 @@
 // }
 // console.log(random);
 
-const password = prompt("please enter a new password");
+// const password = prompt("please enter a new password");
+// if (password.length >= 6) {
+//   if (password.indexOf(' ') === -1) {
+//     console.log("Good job! No space!")
+//   } else {
+//     console.log("Password cannot contain spaces!")
+//   }
+// } else {
+//   console.log("Password too short! Must be +6 characters")
+// }
 
-if (password.length >= 6) {
-  if (password.indexOf(' ') === -1) {
-    console.log("Good job! No space!")
-  } else {
-    console.log("Password cannot contain spaces!")
-  }
 
+const password = prompt("Enter your password");
+if (password.length >= 6 && password.indexOf(' ') === -1) {
+  console.log("Valid Password!")
 } else {
-  console.log("Password too short! Must be +6 characters")
+  console.log("Incorrect format for password!")
 }
